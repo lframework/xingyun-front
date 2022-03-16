366d0ba55e7c38bcd2ffd69a2602b976a703ab45
lframework <lframework@163.com>
2022-03-16 17:30:00 +0800

    重构为antdv

--- README.md
+++ README.md
@@ -2,17 +2,19 @@
 星云ERP是基于SpringBoot框架的中小企业完全开源的ERP。
 
 ### 主要技术框架
-* Vue 2.6.10
-* ElementUI 2.5.15
-* VxeTable 3.3.1
-* VueElementAdmin
+* Vue 2.6.11
+* ant-design-vue 1.7.8
+* vxe-table 3.3.1
+* vue-antd-admin
 
 ### License
 项目使用LGPL3.0许可证，请遵守此许可证的限制条件。
 
 ### 其他说明
-* 目前项目刚刚发布，使用人数很少，暂不提供交流群，Bug请提Issue。
 * 作者是一个只有几年开发经验的后端开发人员，如有错误之处，望斧正。
 * 后端项目Gitee地址：[点此进入][xingyunGitee]
+
+### 注意事项
+老版项目基于ElementUI，已转移到v1分支。
 
 [xingyunGitee]: https://gitee.com/lframework/xingyun
--- vue.config.js
+++ vue.config.js
@@ -1,35 +1,16 @@
-'use strict'
 const path = require('path')
-const defaultSettings = require('./src/settings.js')
-const CompressionPlugin = require('compression-webpack-plugin')
+const webpack = require('webpack')
+const ThemeColorReplacer = require('webpack-theme-color-replacer')
+const { getThemeColors, modifyVars } = require('./src/utils/themeUtil')
+const { resolveCss } = require('./src/utils/theme-color-replacer-extend')
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
 
-function resolve(dir) {
-  return path.join(__dirname, dir)
-}
+const productionGzipExtensions = ['js', 'css']
+const isProd = process.env.NODE_ENV === 'production'
 
-const name = defaultSettings.title || '' // page title
-
-// If your port is set to 80,
-// use administrator privileges to execute the command line.
-// For example, Mac: sudo npm run
-// You can change the port by the following method:
-// port = 9527 npm run dev OR npm run dev --port = 9527
 const port = process.env.port || process.env.npm_config_port || 9527 // dev port
 
-// All configuration item explanations can be find in https://cli.vuejs.org/config/
 module.exports = {
-  /**
-   * You will need to set publicPath if you plan to deploy your site under a sub path,
-   * for example GitHub Pages. If you plan to deploy your site to https://foo.github.io/bar/,
-   * then publicPath should be set to "/bar/".
-   * In most cases please use '/' !!!
-   * Detail: https://cli.vuejs.org/config/#publicpath
-   */
-  publicPath: '/',
-  outputDir: 'dist',
-  assetsDir: 'static',
-  lintOnSave: process.env.NODE_ENV === 'development',
-  productionSourceMap: false,
   devServer: {
     port: port,
     open: true,
@@ -47,51 +28,48 @@ module.exports = {
       }
     }
   },
-  configureWebpack: {
-    // provide the app's title in webpack's name field, so that
-    // it can be accessed in index.html to inject the correct title.
-    name: name,
-    resolve: {
-      alias: {
-        '@': resolve('src')
-      }
-    },
-    plugins: [
-      new CompressionPlugin({
-        filename: '[path].gz[query]',
-        algorithm: 'gzip',
-        test: new RegExp('\\.(js|css)$'),
-        threshold: 10240,
-        minRatio: 0.8, // 默认: 0.8
-        deleteOriginalAssets: false
-      })
-    ]
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'less',
+      patterns: [path.resolve(__dirname, './src/theme/theme.less')]
+    }
   },
-  chainWebpack(config) {
-    // it can improve the speed of the first screen, it is recommended to turn on preload
-    // it can improve the speed of the first screen, it is recommended to turn on preload
-    config.plugin('preload').tap(() => [
-      {
-        rel: 'preload',
-        // to ignore runtime.js
-        // https://github.com/vuejs/vue-cli/blob/dev/packages/@vue/cli-service/lib/config/app.js#L171
-        fileBlacklist: [/\.map$/, /hot-update\.js$/, /runtime\..*\.js$/],
-        include: 'initial'
-      }
-    ])
-
-    // when there are many pages, it will cause too many meaningless requests
-    config.plugins.delete('prefetch')
-
+  configureWebpack: config => {
+    config.entry.app = ['babel-polyfill', 'whatwg-fetch', './src/main.js']
+    config.performance = {
+      hints: false
+    }
+    config.plugins.push(
+      new ThemeColorReplacer({
+        fileName: 'css/theme-colors-[contenthash:8].css',
+        matchColors: getThemeColors(),
+        injectCss: true,
+        resolveCss
+      })
+    )
+    // Ignore all locale files of moment.js
+    config.plugins.push(new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/))
+    // 生产环境下将资源压缩成gzip格式
+    if (isProd) {
+      // add `CompressionWebpack` plugin to webpack plugins
+      config.plugins.push(new CompressionWebpackPlugin({
+        algorithm: 'gzip',
+        test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+        threshold: 10240,
+        minRatio: 0.8
+      }))
+    }
+  },
+  chainWebpack: config => {
     // set svg-sprite-loader
     config.module
       .rule('svg')
-      .exclude.add(resolve('src/icons'))
+      .exclude.add(path.resolve('src/icons'))
       .end()
     config.module
       .rule('icons')
       .test(/\.svg$/)
-      .include.add(resolve('src/icons'))
+      .include.add(path.resolve('src/icons'))
       .end()
       .use('svg-sprite-loader')
       .loader('svg-sprite-loader')
@@ -99,7 +77,6 @@ module.exports = {
         symbolId: 'icon-[name]'
       })
       .end()
-
     config
       .when(process.env.NODE_ENV !== 'development',
         config => {
@@ -107,7 +84,7 @@ module.exports = {
             .plugin('ScriptExtHtmlWebpackPlugin')
             .after('html')
             .use('script-ext-html-webpack-plugin', [{
-            // `runtime` must same as runtimeChunk name. default is `runtime`
+              // `runtime` must same as runtimeChunk name. default is `runtime`
               inline: /runtime\..*\.js$/
             }])
             .end()
@@ -122,13 +99,13 @@ module.exports = {
                   chunks: 'all' // only package third parties that are initially dependent
                 },
                 elementUI: {
-                  name: 'chunk-elementUI', // split elementUI into a single package
+                  name: 'chunk-VXETable', // split elementUI into a single package
                   priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
-                  test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
+                  test: /[\\/]vxe-table[\\/]/ // in order to adapt to cnpm
                 },
                 commons: {
                   name: 'chunk-commons',
-                  test: resolve('src/components'), // can customize your rules
+                  test: path.resolve(__dirname, 'src/components'), // can customize your rules
                   minChunks: 3, //  minimum common number
                   priority: 5,
                   reuseExistingChunk: true
@@ -139,5 +116,27 @@ module.exports = {
           config.optimization.runtimeChunk('single')
         }
       )
-  }
+    // 生产环境下关闭css压缩的 colormin 项，因为此项优化与主题色替换功能冲突
+    if (isProd) {
+      config.plugin('optimize-css')
+        .tap(args => {
+          args[0].cssnanoOptions.preset[1].colormin = false
+          return args
+        })
+    }
+  },
+  css: {
+    loaderOptions: {
+      less: {
+        lessOptions: {
+          modifyVars: modifyVars(),
+          javascriptEnabled: true
+        }
+      }
+    }
+  },
+  publicPath: process.env.VUE_APP_PUBLIC_PATH,
+  outputDir: 'dist',
+  assetsDir: 'static',
+  productionSourceMap: false
 }
