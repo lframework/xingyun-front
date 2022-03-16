9326366816347c77e06ee8f642dbd61512f02ae2
lframework <lframework@163.com>
2022-03-16 18:34:42 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -81,14 +81,6 @@ module.exports = {
       .when(process.env.NODE_ENV !== 'development',
         config => {
           config
-            .plugin('ScriptExtHtmlWebpackPlugin')
-            .after('html')
-            .use('script-ext-html-webpack-plugin', [{
-              // `runtime` must same as runtimeChunk name. default is `runtime`
-              inline: /runtime\..*\.js$/
-            }])
-            .end()
-          config
             .optimization.splitChunks({
               chunks: 'all',
               cacheGroups: {
