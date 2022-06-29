0de702a3d752d95f23c4a126e93fc004c141411d
lframework <lframework@163.com>
2022-06-29 18:30:59 +0800

    Excel导入

--- src/components/Importer/ScImporter.vue
+++ src/components/Importer/ScImporter.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <excel-importer ref="importer" tip-msg="如果编号不存在，那么就新增仓库；如果编号已经存在，那么就修改仓库。" :download-template-url="downloadTemplate" :upload-url="upload" @confirm="e => $emit('confirm', e)" />
+    <excel-importer ref="importer" :tip-msg="'如果编号不存在，那么就新增仓库；如果编号已经存在，那么就修改仓库。\n注：\n地区的格式为：省/市/区（县），例如：北京市/市辖区/朝阳区。文字请参考新增或修改功能中的地区选择。'" :download-template-url="downloadTemplate" :upload-url="upload" @confirm="e => $emit('confirm', e)" />
   </div>
 </template>
 
